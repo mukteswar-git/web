2d9b30834e558dbc6cb52fa0b9adbb653ecdc968
Mukteswar Tripathy <mukteswar.work@gmail.com>
2025-08-08 16:37:19 +0530

    setting up my stater file for a portfolio project using tailwind

---
+++ projects/tailwind4/portfolio/src/main.js
@@ -0,0 +1,15 @@
+import './style.css'
+
+/*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ DARK LIGHT THEME ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ TABS ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
