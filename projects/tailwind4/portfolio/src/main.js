21dfafa3267c6338b04fde054cc90a51d79c8054
Mukteswar Tripathy <mukteswar.work@gmail.com>
2025-08-17 20:01:41 +0530

    portfolio project completed

--- projects/tailwind4/portfolio/src/main.js
+++ projects/tailwind4/portfolio/src/main.js
@@ -49,10 +49,92 @@ function lightMode() {
 }
 
 /*~~~~~~~~~~~~~~~ TABS ~~~~~~~~~~~~~~~*/
+let tabs = document.querySelectorAll('.tab');
+let indicator = document.querySelector('.indicator');
+const all= document.querySelectorAll(".work_card");
+const uiuxs= document.querySelectorAll(".uiux");
+const apps= document.querySelectorAll(".app");
+const branding= document.querySelectorAll(".branding");
+
+indicator.style.width = tabs[0].getBoundingClientRect().width + "px";
+indicator.style.left = tabs[0].getBoundingClientRect().left - tabs[0].parentElement.getBoundingClientRect().left + "px";
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    indicator.style.width = tab.getBoundingClientRect().width + "px";
+    indicator.style.left = tab.getBoundingClientRect().left - tab.parentElement.getBoundingClientRect().left + "px";
+
+    tabs.forEach(t => t.classList.remove("text-whiteColor"));
+    tab.classList.add("text-whiteColor");
+
+    const tabval = tab.getAttribute("data-tabs");
+
+    all.forEach(item => {
+      item.style.display = "none"
+    });
+
+    if(tabval == "uiux"){
+      uiuxs.forEach(item => {
+        item.style.display = "block"
+      });
+    } else if(tabval == "branding"){
+        brandings.forEach(item => {
+        item.style.display = "block"
+      });   
+    } else if(tabval == "app"){
+        apps.forEach(item => {
+        item.style.display = "block"
+      });
+    } else {
+        all.forEach(item => {
+        item.style.display = "block"
+        });
+    }   
+  })
+})
 
 /*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+const scrollHeader = () => {
+  const navbar = document.getElementById("navbar");
+  const aTag = document.querySelectorAll("nav ul li a");
+  const themeToggle = document.getElementById("theme-toggle");
+  const hamburger = document.getElementById("hamburger");
+  const logo = document.getElementById("logo");
+  const whiteLogo = document.getElementById("white-logo");
+
+  if(this.scrollY >= 200) {
+    navbar.classList.add("bg-primaryColor");
+    aTag.forEach((item) => {
+      item.classList.add("text-whiteColor");
+    })
+
+    themeToggle.classList.add("text-whiteColor");
+    hamburger.classList.add("text-whiteColor");
+  } else {
+    navbar.classList.remove("bg-primaryColor");
+    aTag.forEach((item) => {
+      item.classList.remove("text-whiteColor");
+    })
+  }
+
+  themeToggle.classList.remove("text-whiteColor");
+  hamburger.classList.remove("text-whiteColor");
+};
+
+window.addEventListener("scroll", scrollHeader);
 
 /*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+const scrollUp = () => {
+  const scrollUpBtn = document.getElementById("scroll-up");
+
+  if (this.scrollY >= 250) {
+    scrollUpBtn.classList.remove("-bottom-1/2");
+    scrollUpBtn.classList.add("bottom-4");
+  } else {
+    scrollUpBtn.classList.add("-bottom-1/2");
+    scrollUpBtn.classList.remove("bottom-4");
+  }
+}
 
 /*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
 
